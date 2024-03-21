24d71e87c1e6cb18407e3969bf7a1d4990a8b2cd
eldin001 <eldin.balje@icloud.com>
2024-03-21 13:37:42 +0100

    git init

---
+++ script.js
@@ -0,0 +1,43 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe */
+
+let employees = [
+    employee1 = {
+        firstName: ;
+        role: :
+        image: ;
+    },
+    employee2 = {
+        firstName: ;
+        role: :
+        image: ;
+    },
+    employee3 = {
+        firstName: ;
+        role: :
+        image: ;
+    },
+    employee4 = {
+        firstName: ;
+        role: :
+        image: ;
+    },
+    employee5 = {
+        firstName: ;
+        role: :
+        image: ;
+    },
+    employee6 = {
+        firstName: ;
+        role: :
+        image: ;
+    }
+
+
+]
